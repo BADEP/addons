8b76fabafa067eed5a18e66211deb0381bca826f
badep <badep@badep-dev>
2019-02-04 18:42:37 +0000

    fix: default warehouse if no one is defined in customer

--- sale_partner_delivery_info/models/sale.py
+++ sale_partner_delivery_info/models/sale.py
@@ -11,6 +11,6 @@ class SaleOrder(models.Model):
     def onchange_partner_id(self):
         res = super(SaleOrder, self).onchange_partner_id()
         values = {
-            'warehouse_id': self.partner_id and self.partner_id.warehouse_id and self.partner_id.warehouse_id.id or False,
+            'warehouse_id': self.partner_id and self.partner_id.warehouse_id and self.partner_id.warehouse_id.id or self.env['stock.warehouse'].search()[0],
         }
         self.update(values)
