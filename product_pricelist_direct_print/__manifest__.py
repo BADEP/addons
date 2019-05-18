cbd31fbf1faf66611ab0ad163fc6c794cc04e560
BADEP <k.hazam@badep.ma>
2019-05-18 13:57:49 +0000

    port: product_pricelist_direct_print

---
+++ product_pricelist_direct_print/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2017 Carlos Dauden <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Pricelist Direct Print",
+    "summary": "Print price list from menu option, product templates, "
+               "products variants or price lists",
+    "version": "11.0.1.0.0",
+    "category": "Product",
+    "website": "https://www.github.com/OCA/product-attribute",
+    "author": "Tecnativa, BADEP"
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/report_product_pricelist.xml",
+        "wizards/product_pricelist_print_view.xml",
+        "data/mail_template_data.xml",
+    ],
+}
