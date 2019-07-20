6d7ff3d0a341f98c765f3cf2a0da7d12bf54c836
BADEP <k.hazam@badep.ma>
2019-07-20 20:10:19 +0100

    port: sale_fleet

--- sale_fleet/__manifest__.py
+++ sale_fleet/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Sales',
-    'version': '11.0.1',
+    'version': '12.0.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['sale', 'stock_fleet', 'sale_stock'],
@@ -25,9 +25,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/sale_view.xml',
         'views/fleet_view.xml',
     ],
-    'installable': False
 }
--- stock_fleet/__manifest__.py
+++ stock_fleet/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Inventory',
-    'version': '11.0.1',
+    'version': '12.0.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['stock', 'fleet'],
