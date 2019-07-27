45ebfc504c16ef0f943a83aca56b9fe19ac88354
Khalid Hazam <k.hazam@badep.ma>
2019-07-27 02:45:36 +0100

    fix service-worker

--- mail_notify/static/src/js/service-worker.js
+++ mail_notify/static/src/js/service-worker.js
@@ -1,4 +1,4 @@
-/mportScripts('https://www.gstatic.com/firebasejs/6.3.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/6.3.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/6.3.1/firebase-messaging.js');
 firebase.initializeApp();
 
