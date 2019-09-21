2e248c271ffce6ddb40e6cae493c139b5853e65f
Khalid Hazam <k.hazam@badep.ma>
2019-09-21 16:41:52 +0100

    fixe deps

--- sale_total_delivered_invoiced/__manifest__.py
+++ sale_total_delivered_invoiced/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '10.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['account', 'fleet'],
+    'depends': ['sale'],
     'images': ['static/scr/img/banner.png'],
     'license': 'AGPL-3',
 
