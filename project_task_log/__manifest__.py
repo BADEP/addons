24e69a26f3d717e6b14c2d93b8aafff6bd2ca4bb
Khalid Hazam <k.hazam@badep.ma>
2019-11-13 16:35:42 +0100

    [add] project_task_log module

---
+++ project_task_log/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Project Task Log",
+
+    'summary': """
+        Log Project Task changes: Assignation and stage""",
+
+    'description': """
+    """,
+
+    'author': "BADEP",
+    'website': "https://badep.ma",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Project',
+
+    # any module necessary for this one to work correctly
+    'depends': ['project', 'web_widget_time_delta'],
+    'images': ['static/scr/img/banner.png'],
+    'license': 'AGPL-3',
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'security/project_task_log_security.xml',
+        'views/task_view.xml'
+    ],
+    'installable': True,
+}
