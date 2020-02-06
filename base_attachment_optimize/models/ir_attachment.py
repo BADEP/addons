b70a85ebca0e1a24e396dc66b79f25ad09e9d320
Khalid Hazam <k.hazam@badep.ma>
2020-02-06 15:59:23 +0100

    [IMP] separate algorithme from cron

--- base_attachment_optimize/models/ir_attachment.py
+++ base_attachment_optimize/models/ir_attachment.py
@@ -20,34 +20,37 @@ class IrAttachment(models.Model):
 
     optimized = fields.Boolean('Optimized', default=False, readonly=True, store=True)
 
+    def optimize(self):
+        pdfquality = self.env['ir.config_parameter'].sudo().get_param('base_attachment_optimize.pdf_quality')
+        for att in self:
+            try:
+                path = att._full_path(att.store_fname)
+                cmd = "gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/" + pdfquality + " -dNOPAUSE -dQUIET -dBATCH -sOutputFile=/tmp/output.pdf " + path
+                os.system(cmd)
+                # args = [
+                #     "-dNOPAUSE", "-dBATCH", "-dQUIET",
+                #     "-sDEVICE=pdfwrite",
+                #     '-dCompatibilityLevel=1.5',
+                #     "-dPDFSETTINGS=/" + pdfquality,
+                #     "-sOutputFile=/tmp/output.pdf",
+                #     "-f", path
+                # ]
+                # encoding = locale.getpreferredencoding()
+                # args = [a.encode(encoding) for a in args]
+                # ghostscript.Ghostscript(*args)
+                bin_datas = open('/tmp/output.pdf', 'rb').read()
+                if att.file_size > len(bin_datas):
+                    att.write({'datas': base64.b64encode(bin_datas)})
+            except Exception:
+                pass
+        self.write({'optimized': True})
+
     @api.model
     def cron_compress(self):
         if self.env['ir.config_parameter'].sudo().get_param('base_attachment_optimize.optimize_pdf'):
             attachments = self.search([('mimetype', '=', 'application/pdf'), ('type', '=', 'binary'), ('optimized', '=', False)],
                                        limit=int(self.env['ir.config_parameter'].sudo().get_param('base_attachment_optimize.batch_size')))
-            pdfquality = self.env['ir.config_parameter'].sudo().get_param('base_attachment_optimize.pdf_quality')
-            for att in attachments:
-                try:
-                    path = att._full_path(att.store_fname)
-                    cmd = "gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/" + pdfquality + " -dNOPAUSE -dQUIET -dBATCH -sOutputFile=/tmp/output.pdf " + path
-                    os.system(cmd)
-                    # args = [
-                    #     "-dNOPAUSE", "-dBATCH", "-dQUIET",
-                    #     "-sDEVICE=pdfwrite",
-                    #     '-dCompatibilityLevel=1.5',
-                    #     "-dPDFSETTINGS=/" + pdfquality,
-                    #     "-sOutputFile=/tmp/output.pdf",
-                    #     "-f", path
-                    # ]
-                    # encoding = locale.getpreferredencoding()
-                    # args = [a.encode(encoding) for a in args]
-                    # ghostscript.Ghostscript(*args)
-                    bin_datas = open('/tmp/output.pdf','rb').read()
-                    if att.file_size > len(bin_datas):
-                        att.write({'datas': base64.b64encode(bin_datas)})
-                except Exception:
-                    pass
-            attachments.write({'optimized': True})
+            attachments.optimize()
         return True
 
     def _inverse_datas(self):
