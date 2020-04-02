4e450bf4406747ad8b314a5ee93c8f8765dc9e65
Khalid Hazam <k.hazam@badep.ma>
2020-04-02 15:13:59 +0100

    [FIX] mobile_android

--- mobile_android/static/src/js/session.js
+++ mobile_android/static/src/js/session.js
@@ -1,10 +1,15 @@
 odoo.define('mail_notify.BusService', function (require) {
     "use strict";
     var Session = require('web.Session');
+    var ajax = require('web.ajax');
     console.log('Android: session declaration');
     Session.include({
         get_file: function(options) {
             if (typeof Android != "undefined") {Android.getFile(options);}
+            if (this.override_session) {
+                options.data.session_id = this.session_id;
+            }
+            options.session = this;
             return ajax.get_file(options);
         }
     });
