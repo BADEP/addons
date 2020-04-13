69e273b32d6e5384ae5a101a909e06ba18376bab
Khalid Hazam <k.hazam@badep.ma>
2020-04-13 16:52:00 +0100

    [UPD] move partner_statement_fix from private repo

---
+++ partner_statement_fix/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Partner Statement Fix',
+    'version': '12.0.1.0.1',
+    'category': 'Accounting & Finance',
+    'summary': 'OCA Financial Reports',
+    'author': "BADEP",
+    'website': 'https://badep.ma',
+    'license': 'AGPL-3',
+    'depends': [
+        'partner_statement',
+    ],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
---
+++ partner_statement_fix/report/__init__.py
@@ -0,0 +1,1 @@
+from . import activity_statement
