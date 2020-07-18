e4c8e243864521d225f55f4f2bc1adaf04f606c5
Khalid Hazam <k.hazam@badep.ma>
2020-07-18 15:05:03 +0100

    [12.0][ADD] uom_dimension
    (cherry picked from commit 9c9718ad05ba715c8c15298a5598e419266d1ee5)

--- uom_dimension/models/uom_uom.py
+++ uom_dimension/models/uom_uom.py
@@ -1,27 +1,45 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-import numpy
+from odoo import models, fields, api, exceptions, _
+import time, datetime, dateutil, numpy
+from odoo.tools import safe_eval
 
 DEFAULT_PYTHON_CODE = """# Available variables:
 #  - env: Odoo Environment
+#  - record: Uom record
 #  - dimension_values: dict of dimension values with dimension key
-#  - dimension_ids: record set of the UoM dimensions
-#  - time, datetime, dateutil, timezone, numpy: useful Python libraries
-#  - log: log(message, level='info'): logging function to record debug information in ir.logging table
+#  - time, datetime, dateutil, numpy: useful Python libraries
 #  - Warning: Warning Exception to use with raise
 #  - result: return result
 #  Example (Total perimeter of a window with regard of the quantity wanted): result = 2 * dimension_values[dimension_ids[0].id] * (dimension_values[dimension_ids[1].id] + dimension_values[dimension_ids[2].id])
 #  Example (Total surface of a window with regard of the quantity wanted): result = numpy.prod(dimension_values.values()) \n\n\n\n"""
 
-class ProductUom(models.Model):
-    _inherit = "uom.uom"
+class UomUom(models.Model):
+    _inherit = 'uom.uom'
     
-    dimension_ids = fields.One2many('uom.dimension', 'uom_id', copy=True)
-    type = fields.Selection([('simple', 'Simple', 'code', 'Code')])
+    dimension_ids = fields.One2many('uom.dimension', 'parent_uom_id', copy=True)
+    type = fields.Selection([('multiply', 'Multiply', 'code', 'Code')])
     code = fields.Text(string='Python Code', default=DEFAULT_PYTHON_CODE)
 
-class ProductUomDimension(models.Model):
+    def eval_values(self, dimension_values):
+        for uom in self:
+            eval_context = {
+                'env': self.env,
+                'time': time,
+                'datetime': datetime,
+                'dateutil': dateutil,
+                'numpy': numpy,
+                'Warning': exceptions.Warning,
+                'record': uom,
+                'dimension_values': dimension_values,
+                'result': 0,
+            }
+            try:
+                safe_eval(self.amount_python_compute, eval_context, mode='exec', nocopy=True)
+                return float(eval_context['result'])
+            except:
+                raise exceptions.UserError(_('Wrong python code defined for uom %s.') % (uom.name))
+
+class UomDimension(models.Model):
     _name = 'uom.dimension'
     name = fields.Char(required=True)
     uom_id = fields.Many2one('uom.uom', required=True)
+    parent_uom_id = fields.Many2one('uom.uom', required=True)
