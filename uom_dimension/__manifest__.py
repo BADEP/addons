9c9718ad05ba715c8c15298a5598e419266d1ee5
Khalid Hazam <k.hazam@badep.ma>
2020-07-18 15:05:03 +0100

    [12.0][ADD] uom_dimension

--- uom_dimension/__manifest__.py
+++ uom_dimension/__manifest__.py
@@ -1,31 +1,20 @@
-# -*- coding: utf-8 -*-
 {
     'name': "Dimensions in Product UoM",
 
     'summary': """
         Eeach Unit of Measure has its dimensions. This is a technical module, functional behavior can be found in corresponding modules (stock_uom_dimension, sale_uom_dimension, purchase_uom_dimension).""",
-
-    'description': """
-    """,
-
     'author': "BADEP",
     'website': "https://badep.ma",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
     'category': 'Inventory',
-    'version': '11.0.1',
-
-    # any module necessary for this one to work correctly
+    'version': '12.0.1.0.1',
     'depends': ['uom'],
     'images': ['static/scr/img/banner.png'],
     'license': 'AGPL-3',
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/uom_view.xml',
     ],
-    'installable': False,
+    'installable': True,
+    'price': 20,
+    'currency': 'EUR'
 }
