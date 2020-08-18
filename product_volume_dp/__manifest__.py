39ce0448b6ff3b8f3f9988d60b188f5782382782
Khalid Hazam <k.hazam@badep.ma>
2020-08-18 16:30:03 +0100

    [ADD] product_volume_dp

---
+++ product_volume_dp/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Decimal Precision in Product Volume",
+
+    'summary': """ Add a new Decimal Precision for the volume field in product. Backport from v13 """,
+
+    'description': """
+    """,
+
+    'author': "BADEP",
+    'website': "https://badep.ma",
+
+    'category': 'Hidden',
+    'version': '12.0.0.1',
+
+    'depends': ['product'],
+    'data': [
+        'data/dp_data.xml'
+    ]
+}
