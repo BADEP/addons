97d2e2c0f048cf6919e6c476e8daf0366b81306d
Khalid Hazam <k.hazam@badep.ma>
2020-08-31 15:20:07 +0100

    [ADD] base_action_boto3

---
+++ base_action_boto3/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "boto3 in server actions",
+
+    'summary': """Allow the use of boto3 lib in server actions
+        """,
+
+    'description': """
+    """,
+
+    'author': "BADEP",
+    'website': "https://badep.ma",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Technical Settings',
+    'version': '12.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base'
+    ],
+    'external_dependencies': {
+        'python': [
+            'boto3',
+        ],
+    },
+    'installable': True,
+    'auto_install': True,
+}
