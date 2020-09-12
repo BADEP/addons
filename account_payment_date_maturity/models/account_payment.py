20c9232b18ba8e359a48cef6f78ce73d48ec2906
Khalid Hazam <k.hazam@badep.ma>
2020-09-12 17:18:14 +0100

    [MIG][13.0] account_payment_date_maturity

--- account_payment_date_maturity/models/account_payment.py
+++ account_payment_date_maturity/models/account_payment.py
@@ -6,11 +6,13 @@ from odoo import models, fields
 class AccountPayment(models.Model):
     _inherit = "account.payment"
     
-    date_maturity = fields.Date(string="Maturity Date")
+    date_maturity = fields.Date(string="Due Date")
 
-    def _get_shared_move_line_vals(self, debit, credit, amount_currency, move_id, invoice_id=False):
-        res = super(AccountPayment, self)._get_shared_move_line_vals(debit, credit, amount_currency, move_id, invoice_id=invoice_id)
-        res.update({
-            'date_maturity': self.date_maturity if self.date_maturity else self.payment_date
-        })
+    def _prepare_payment_moves(self):
+        res = super(AccountPayment, self)._prepare_payment_moves()
+        for move_val in res:
+            for move_line_val in move_val['line_ids']:
+                move_line_val[2].update({
+                    'date_maturity': self.date_maturity if self.date_maturity and move_line_val[2]['debit'] else self.payment_date
+                })
         return res
