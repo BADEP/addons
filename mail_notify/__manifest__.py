2ab16c142a22836c478dad833b54d06e74eb0ec9
Khalid Hazam <k.hazam@badep.ma>
2020-09-14 15:34:52 +0100

    [FIX] mail_notify image and description

--- mail_notify/__manifest__.py
+++ mail_notify/__manifest__.py
@@ -11,11 +11,11 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Discuss',
-    'version': '13.0.1.0',
+    'version': '12.0.1.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['mail_bot', 'web'],
-    'images': ['static/scr/img/banner.png'],
+    'depends': ['mail', 'web'],
+    'images': ['static/src/img/banner.png'],
     'license': 'AGPL-3',
     'external_dependencies': {
         'python': [
