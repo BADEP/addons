ac31faec1d675e12d34141b601bcb96515cb3c40
Khalid Hazam <k.hazam@badep.ma>
2020-09-29 19:05:03 +0100

    [MIG] mail_notify

--- mail_notify/__manifest__.py
+++ mail_notify/__manifest__.py
@@ -11,7 +11,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Discuss',
-    'version': '13.0.1.0',
+    'version': '14.0.1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['mail_bot', 'web'],
