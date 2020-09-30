87502b8b14609a85285f291fe3bd30e85fa978be
Khalid Hazam <k.hazam@badep.ma>
2020-09-30 15:04:34 +0100

    [MIG] account_invoice_fleet

--- account_invoice_fleet/models/fleet.py
+++ account_invoice_fleet/models/fleet.py
@@ -19,19 +19,19 @@ class FleetVehicle(models.Model):
 
     @api.depends('invoices')
     def get_invoicing(self):
-        self.invoices_sale_amount = sum(invoice.amount_total_signed for invoice in self.invoices.filtered(lambda s: s.state == 'posted' and s.type in ('out_invoice', 'out_refund')))
-        self.invoices_sale_count = len(self.invoices.filtered(lambda s: s.state == 'posted' and s.type in ('out_invoice', 'out_refund')))
-        self.invoices_purchase_amount = sum(invoice.amount_total_signed for invoice in self.invoices.filtered(lambda s: s.state == 'posted' and s.type in ('in_invoice', 'in_refund')))
-        self.invoices_purchase_count = len(self.invoices.filtered(lambda s: s.state == 'posted' and s.type in ('in_invoice', 'in_refund')))
+        self.invoices_sale_amount = sum(invoice.amount_total_signed for invoice in self.invoices.filtered(lambda s: s.state == 'posted' and s.move_type in ('out_invoice', 'out_refund')))
+        self.invoices_sale_count = len(self.invoices.filtered(lambda s: s.state == 'posted' and s.move_type in ('out_invoice', 'out_refund')))
+        self.invoices_purchase_amount = sum(invoice.amount_total_signed for invoice in self.invoices.filtered(lambda s: s.state == 'posted' and s.move_type in ('in_invoice', 'in_refund')))
+        self.invoices_purchase_count = len(self.invoices.filtered(lambda s: s.state == 'posted' and s.move_type in ('in_invoice', 'in_refund')))
 
     def act_show_sale_invoices(self):
         action = self.env.ref('account.action_move_out_invoice_type').read()[0]
-        action['domain'] = "[('id','in',["+','.join(map(str, self.invoices.ids))+"]),('type', 'in', ('out_invoice', 'out_refund'))]"
-        action['context'] = {'default_vehicle': self.id, 'default_driver': self.driver_id.id, 'default_type': 'out_invoice'}
+        action['domain'] = "[('id','in',["+','.join(map(str, self.invoices.ids))+"]),('move_type', 'in', ('out_invoice', 'out_refund'))]"
+        action['context'] = {'default_vehicle': self.id, 'default_driver': self.driver_id.id, 'default_move_type': 'out_invoice'}
         return action
 
     def act_show_purchase_invoices(self):
         action = self.env.ref('account.action_move_in_invoice_type').read()[0]
-        action['domain'] = "[('id','in',["+','.join(map(str, self.invoices.ids))+"]),('type', 'in', ('in_invoice', 'in_refund'))]"
-        action['context'] = {'default_vehicle': self.id, 'default_driver': self.driver_id.id, 'default_type': 'in_invoice'}
+        action['domain'] = "[('id','in',["+','.join(map(str, self.invoices.ids))+"]),('move_type', 'in', ('in_invoice', 'in_refund'))]"
+        action['context'] = {'default_vehicle': self.id, 'default_driver': self.driver_id.id, 'default_move_type': 'in_invoice'}
         return action
