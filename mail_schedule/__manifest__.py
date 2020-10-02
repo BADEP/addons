328d587c681d66fbb99347fa528c416003ea8fe2
Khalid Hazam <k.hazam@badep.ma>
2020-10-02 15:03:32 +0100

    [ADD] mail_schedule

---
+++ mail_schedule/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Schedule email sending",
+
+    'summary': """Schedule mail sent for a later date""",
+
+    'author': "BADEP",
+    'website': "https://badep.ma",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Discuss',
+    'version': '12.0.1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['mail', 'web'],
+    'images': ['static/src/img/banner.png'],
+    'license': 'AGPL-3',
+    # always loaded
+    'data': [
+        'views/mail_views.xml'
+    ],
+    'price': 29.00,
+    'currency': 'USD',
+}
