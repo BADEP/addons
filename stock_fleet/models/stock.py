523f293039cc6b958021157df5672a870f13f42c
Khalid Hazam <k.hazam@badep.ma>
2020-10-21 12:21:55 +0100

    [IMP] stock_fleet add odometer

--- stock_fleet/models/stock.py
+++ stock_fleet/models/stock.py
@@ -6,6 +6,7 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
     vehicle = fields.Many2one('fleet.vehicle', domain=[('picking_ok','=',True)])
     driver = fields.Many2one('res.partner')
+    odometer = fields.Float('Kilométrage', related='vehicle.odometer', store=True, readonly=False)
     
     @api.onchange('vehicle')
     def set_driver(self):
@@ -18,4 +19,4 @@ class StockMove(models.Model):
     _inherit = 'stock.move'
     vehicle = fields.Many2one('fleet.vehicle', related='picking_id.vehicle', store=True, readonly=True)
     driver = fields.Many2one('res.partner', related='picking_id.driver', store=True, readolny=True)
-    
+    odometer = fields.Float('Kilométrage', related='picking_id.odometer', store=True, readolny=True)
