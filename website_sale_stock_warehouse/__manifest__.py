2ec9c001bba3534b9f903380507859c9fcfc9e1d
Khalid Hazam <k.hazam@badep.ma>
2020-11-03 17:08:20 +0100

    [ADD] website_sale_stock_warehouse

---
+++ website_sale_stock_warehouse/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Website Sale Stock Warehouse",
+
+    'summary': """
+        Choose proper warehouse when sale order is generated from website""",
+    'author': "BADEP",
+    'website': "https://badep.ma",
+    'category': 'Sales Management',
+    'version': '12.0.1.0.1',
+    'images': ['static/src/img/banner.png'],
+    'license': 'AGPL-3',
+    'depends': ['website_sale_stock'],
+    'data': [
+             'views/res_config_settings_views.xml',
+             ],
+    'auto_install': True,
+    'installable': True,
+    'price': 30,
+    'currency': 'USD'
+}
