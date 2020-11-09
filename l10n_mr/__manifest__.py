dfb119bb778d4dfc3f5e449509e40b0eab628c20
Khalid Hazam <k.hazam@badep.ma>
2020-11-09 15:28:07 +0100

    [ADD] l10n_mr initial version

---
+++ l10n_mr/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name' : 'Plan comptable Mauritanie',
+    'version' : '12.0.0.1',
+    'author' : 'Africa Performances, BADEP',
+    'category' : 'Localization/Account Charts',
+    'description': 	"""
+					Accounting chart PCM for Mauritania.
+					- Account type from Annual Financial Statement
+					- VAT Taxes and Fiscal Position (TO DO)
+					""",
+    'website': 'https://badep.ma/',
+    'depends' : ['account', 'base_vat'],
+    'demo' : [],
+    'data' : [
+        'data/account_data.xml',
+        'data/l10n_mr_chart_data.xml',
+        'data/account_tax_data.xml',
+        'data/account_chart_template_data.xml',
+    ],
+    'installable': True,
+}
