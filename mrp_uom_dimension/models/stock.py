b4568bb727b9bd56ec3a6625b6e2bd1fc6fc0e13
Khalid Hazam <k.hazam@badep.ma>
2020-11-09 15:50:22 +0100

    [ADD] mrp_uom_dimension installable

---
+++ mrp_uom_dimension/models/stock.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models
+
+class StockRule(models.Model):
+    _inherit = 'stock.rule'
+
+    def _prepare_mo_vals(self, product_id, product_qty, product_uom, location_id, name, origin, values, bom):
+        res = super(StockRule, self)._prepare_mo_vals(product_id, product_qty, product_uom, location_id, name, origin, values, bom)
+        res.update({
+            'product_dimension_qty': values.get('product_dimension_qty', 0),
+            'dimension_ids': values.get('dimension_ids', [])
+        })
+        return res
