a91672a8ba19f54af449238350f540e2e24b6b00
Khalid Hazam <k.hazam@badep.ma>
2020-11-20 13:18:35 +0100

    [IMP] mrp_uom_dimension improve change wizard

--- mrp_uom_dimension/models/mrp.py
+++ mrp_uom_dimension/models/mrp.py
@@ -1,23 +1,24 @@
-# -*- coding: utf-8 -*-
-
 from odoo import models, fields, api, _
-import odoo.addons.decimal_precision as dp
 
-class MrpBomLine(models.Model):
-    _inherit = 'mrp.bom.line'
-    dimension_ids = fields.Many2many('uom.dimension',  domain="[('product_uom', '=', parent.product_uom)]")
-        
 class MrpProduction(models.Model):
     _inherit = "mrp.production"
-    dimension_ids = fields.One2many('mrp.production.dimension','mrp_production')
+    dimension_ids = fields.One2many('mrp.production.dimension', 'mrp_production')
     product_dimension_qty = fields.Integer('Nombre', required=True, default=0)
+
+    @api.onchange('product_dimension_qty', 'dimension_ids')
+    def onchange_dimension_ids(self):
+        if self.dimension_ids:
+            qty = self.mo_id.product_uom_id.eval_values(
+                dict([(d.dimension_id.id, d.quantity) for d in self.dimension_ids]), self.product_dimension_qty)
+            if qty != self.product_uom_qty:
+                self.product_uom_qty = qty
 
 class MrpProductionDimension(models.Model):
     _name = "mrp.production.dimension"
 
     dimension_id = fields.Many2one('uom.dimension', required=True, ondelete='cascade')
     quantity = fields.Float('Quantité', required=True)
-    mrp_production = fields.Many2one('mrp.production', required=True, ondelete='cascade')
+    mrp_production = fields.Many2one('mrp.production', ondelete='cascade')
     name = fields.Char(compute='get_name', store=True)
     display_name = fields.Char(compute='get_name', store=True)
 
@@ -27,5 +28,37 @@ class MrpProductionDimension(models.Model):
             rec.display_name = rec.dimension_id.name + ': ' + str(rec.quantity)
             rec.name = rec.dimension_id.name + ': ' + str(rec.quantity)
 
-class MrpBom(models.Model):
-    _inherit = 'mrp.bom'
+
+class ChangeProductionQty(models.TransientModel):
+    _inherit = 'change.production.qty'
+
+    dimension_ids = fields.Many2many('mrp.production.dimension', 'change_production_qty_id')
+    product_dimension_qty = fields.Integer('Nombre', required=True)
+
+    @api.model
+    def default_get(self, fields):
+        res = super(ChangeProductionQty, self).default_get(fields)
+        if res.get('mo_id'):
+            res['product_dimension_qty'] = self.env['mrp.production'].browse(res['mo_id']).product_qty
+            res['dimension_ids'] = [(0, 0, {'dimension_id': d.dimension_id.id, 'quantity': d.quantity}) for d in
+                                    self.env['mrp.production'].browse(res['mo_id']).dimension_ids]
+        return res
+
+    @api.onchange('product_dimension_qty', 'dimension_ids')
+    def onchange_dimension_ids(self):
+        if self.dimension_ids:
+            qty = self.mo_id.product_uom_id.eval_values(
+                dict([(d.dimension_id.id, d.quantity) for d in self.dimension_ids]), self.product_dimension_qty)
+            if qty != self.product_qty:
+                self.product_qty = qty
+
+    @api.multi
+    def change_prod_qty(self):
+        res = super().change_prod_qty()
+        for wizard in self:
+            wizard.mo_id.dimension_ids.unlink()
+            wizard.mo_id.write({
+                'product_dimension_qty': wizard.product_dimension_qty,
+                'dimension_ids': [(0, 0, {'dimension_id': d.dimension_id.id, 'quantity': d.quantity}) for d in wizard.dimension_ids]
+            })
+        return res
