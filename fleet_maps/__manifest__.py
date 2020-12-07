d43ac7e62c16822f09398b0f1e5627a5c5d8d0b8
Khalid Hazam <k.hazam@badep.ma>
2020-12-07 19:21:13 +0000

    [IMP] publish modules to public repo

---
+++ fleet_maps/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Fleet Maps',
+    'version': '12.0.1.0.2',
+    'author': 'Yopi Angi, BADEP',
+    'license': 'AGPL-3',
+    'maintainer': 'Yopi Angi <yopiangi@gmail.com>, Khalid Hazam <k.hazam@badep.ma>',
+    'support': 'yopiangi@gmail.com',
+    'category': 'Fleet',
+    'description': """
+Fleet Maps
+========
+
+Show your leads and pipelines on map
+""",
+    'depends': [
+        'fleet',
+        'web_google_maps'
+    ],
+    'data': [
+        'views/fleet_vehicle.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'uninstall_hook': 'uninstall_hook',
+}
---
+++ sale_order_maps/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sale Order Maps',
+    'version': '12.0.1.0.0',
+    'author': 'Yopi Angi, BADEP',
+    'license': 'AGPL-3',
+    'maintainer': 'Yopi Angi <yopiangi@gmail.com>, Khalid Hazam <k.hazam@badep.ma>',
+    'support': 'yopiangi@gmail.com',
+    'category': 'Sales',
+    'description': """
+Sale Order Maps
+========
+
+Show your Sale orders  on map view in regards to delivery address
+""",
+    'depends': [
+        'sale',
+        'contacts_maps'
+    ],
+    'data': [
+        'views/sale_views.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'uninstall_hook': 'uninstall_hook',
+}
