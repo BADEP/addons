3d6b952d4871caaa61757360c1595bf413f8e68d
Khalid Hazam <k.hazam@badep.ma>
2020-12-15 09:35:55 +0000

    [ADD] stock_tier_validation

---
+++ stock_tier_validation/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Open Source Integrators
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Tier Validation",
+    "summary": "Extends the functionality of Stock Pickings to "
+               "support a tier validation process.",
+    "version": "12.0.1.0.0",
+    "category": "Inventory",
+    "website": "https://badep.ma",
+    "author": "BADEP, Open Source Integrators, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "stock",
+        "base_tier_validation",
+    ],
+    "data": [
+        "views/stock_picking_view.xml",
+    ],
+}
---
+++ stock_tier_validation/models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2019 Open Source Integrators
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import stock_picking
+from . import tier_definition
