d705694b0ddfbae2378a42ef6ec039741b2a9065
Khalid Hazam <k.hazam@badep.ma>
2020-12-15 17:14:27 +0000

    [WIP] stock_tier_validation working state

--- stock_tier_validation/models/stock_picking.py
+++ stock_tier_validation/models/stock_picking.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Open Source Integrators
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import models, fields
 
 
 class StockPicking(models.Model):
@@ -9,3 +9,10 @@ class StockPicking(models.Model):
     _inherit = ['stock.picking', 'tier.validation']
     _state_from = ['assigned', 'waiting', 'confirmed']
     _state_to = ['done']
+
+    def button_validate(self):
+        self.write({'state': 'done'})
+        return super().button_validate()
+
+    def _notify_accepted_reviews(self):
+        return super(StockPicking, self.sudo())._notify_accepted_reviews()
