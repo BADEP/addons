7eb70571682f61b8d09a73af181bafd64ef43e99
Khalid Hazam <k.hazam@badep.ma>
2021-05-06 14:19:08 +0000

    [ADD] purchase_uom_dimension

---
+++ purchase_uom_dimension/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': "UoM Dimensions in purchases",
+
+    'summary': """
+        Allows the use of UoM dimensions in purchases.""",
+    'author': "BADEP",
+    'website': "https://badep.ma",
+    'category': 'purchases Management',
+    'version': '12.0.1.0.1',
+    'images': ['static/src/img/banner.png'],
+    'license': 'AGPL-3',
+    'depends': ['purchase', 'uom_dimension'],
+    'data': [
+             'views/purchase_views.xml',
+             'report/purchase_report_templates.xml',
+             'security/ir.model.access.csv',
+             ],
+    'auto_install': True,
+    'installable': True,
+    'price': 30,
+    'currency': 'EUR'
+}
