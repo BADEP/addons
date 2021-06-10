1bf30315e4875fd5bc151c4dab59837e84ec5b2c
Khalid Hazam <k.hazam@badep.ma>
2021-06-10 18:19:52 +0100

    [IMP] add distribution lines in pickings

--- sale_mrp_lock/models/mrp_production.py
+++ sale_mrp_lock/models/mrp_production.py
@@ -6,6 +6,6 @@ class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
     def write(self, vals):
-        for rec in self.filtered(lambda i: i.sale_id and i.sale_id.state == 'done'):
+        for rec in self.sudo().filtered(lambda i: i.sale_id and i.sale_id.state == 'done'):
             raise UserError('L\'ordre de fabrication est bloquée par la commande ' + rec.sale_id.name)
         return super().write(vals)
