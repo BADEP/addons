f03295c6212be117ea9fe18a57148d13dfbaa93b
Khalid Hazam <k.hazam@badep.ma>
2021-06-15 13:00:30 +0100

    [WIP] sale_stock_manual_launch_mrp calculate procurement qty

--- sale_stock_manual_launch_mrp/models/sale.py
+++ sale_stock_manual_launch_mrp/models/sale.py
@@ -1,11 +1,20 @@
-from odoo import models, api
+from odoo import models, api, fields
+from odoo.addons import decimal_precision as dp
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
+    procurement_qty_manual = fields.Float(string="Qté lancée", digits=dp.get_precision('Product Unit of Measure'), store=False)
+
     @api.one
     def get_dummy_qty(self):
         return self.product_uom_qty - self.procurement_qty
+
+    def action_launch_procurement(self):
+        bom = self.env['mrp.bom']._bom_find(product=self.product_id)
+        if bom and bom.type == 'phantom' and self.env.context.get('qty_to_launch'):
+            self.procurement_qty_manual = self.env.context['qty_to_launch']
+        return super().action_launch_procurement()
 
     def _get_procurement_quantity(self):
         for rec in self:
@@ -15,6 +24,6 @@ class SaleOrderLine(models.Model):
                 outgoing_moves = moves.filtered(lambda m: m.location_dest_id.usage == "customer" and (
                             not m.origin_returned_move_id or (m.origin_returned_move_id and m.to_refund)))
                 if outgoing_moves:
-                    rec.procurement_qty = (rec.qty_delivered_manual or rec.product_uom_qty) - self.env.context.get('qty_to_launch', 0)
+                    rec.procurement_qty = rec.procurement_qty_manual - self.env.context.get('qty_to_launch', 0)
             else:
                 super(SaleOrderLine, rec)._get_procurement_quantity()
