8315343966adfc2e5b1f17bacc625ece53fd5c7e
Khalid Hazam <k.hazam@badep.ma>
2021-06-28 12:58:05 +0100

    [MOVE] base_import_writable to public repos

---
+++ base_import_writable/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Import writable fields",
+
+    'summary': """
+        """,
+
+    'description': """
+    """,
+
+    'author': "BADEP",
+    'website': "https://badep.ma",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Technical Settings',
+    'version': '14.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base_import',
+    ],
+
+    'installable': True,
+    'auto_install': False,
+    'application': False
+}
---
+++ base_import_writable/models/base_import.py
@@ -0,0 +1,68 @@
+
+import codecs
+import itertools
+import logging
+
+
+from odoo import api, fields, models
+from odoo.tools.translate import _
+
+FIELDS_RECURSION_LIMIT = 3
+
+class Import(models.TransientModel):
+
+    _inherit = 'base_import.import'
+
+    @api.model
+    def get_fields(self, model, depth=FIELDS_RECURSION_LIMIT):
+        Model = self.env[model]
+        importable_fields = [{
+            'id': 'id',
+            'name': 'id',
+            'string': _("External ID"),
+            'required': False,
+            'fields': [],
+            'type': 'id',
+        }]
+        if not depth:
+            return importable_fields
+
+        model_fields = Model.fields_get()
+        blacklist = models.MAGIC_COLUMNS + [Model.CONCURRENCY_CHECK_FIELD]
+        for name, field in model_fields.items():
+            if name in blacklist:
+                continue
+            if field.get('deprecated', False) is not False:
+                continue
+            if field.get('readonly') and field.get('compute') and not field.get('inverse'):
+                states = field.get('states')
+                if not states:
+                    continue
+                # states = {state: [(attr, value), (attr2, value2)], state2:...}
+                if not any(attr == 'readonly' and value is False
+                           for attr, value in itertools.chain.from_iterable(states.values())):
+                    continue
+            field_value = {
+                'id': name,
+                'name': name,
+                'string': field['string'],
+                # Y U NO ALWAYS HAS REQUIRED
+                'required': bool(field.get('required')),
+                'fields': [],
+                'type': field['type'],
+            }
+
+            if field['type'] in ('many2many', 'many2one'):
+                field_value['fields'] = [
+                    dict(field_value, name='id', string=_("External ID"), type='id'),
+                    dict(field_value, name='.id', string=_("Database ID"), type='id'),
+                ]
+            elif field['type'] == 'one2many':
+                field_value['fields'] = self.get_fields(field['relation'], depth=depth-1)
+                if self.user_has_groups('base.group_no_one'):
+                    field_value['fields'].append({'id': '.id', 'name': '.id', 'string': _("Database ID"), 'required': False, 'fields': [], 'type': 'id'})
+
+            importable_fields.append(field_value)
+
+        # TODO: cache on model?
+        return importable_fields
