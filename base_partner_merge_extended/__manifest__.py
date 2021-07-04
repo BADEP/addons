c5864fbe1205877bebce7f340f43a016ece6cfb8
Khalid Hazam <k.hazam@badep.ma>
2021-07-04 20:59:07 +0100

    [FF] base_partner_merge_extended

--- base_partner_merge_extended/__manifest__.py
+++ base_partner_merge_extended/__manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Technical',
-    'version': '12.0.0.1',
+    'version': '13.0.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
--- base_partner_merge_extended/wizard/base_partner_merge.py
+++ base_partner_merge_extended/wizard/base_partner_merge.py
@@ -1,11 +1,71 @@
-from odoo import fields, models, api
-
+from odoo import fields, models, api, _
+from odoo.exceptions import UserError
+import logging
+_logger = logging.getLogger('base.partner.merge')
 
 class MergePartnerAutomatic(models.TransientModel):
     _inherit = 'base.partner.merge.automatic.wizard'
 
     group_by_phone = fields.Boolean('Phone')
     group_by_mobile = fields.Boolean('Mobile')
+
+    def _merge(self, partner_ids, dst_partner=None, extra_checks=True):
+        """ private implementation of merge partner
+            :param partner_ids : ids of partner to merge
+            :param dst_partner : record of destination res.partner
+            :param extra_checks: pass False to bypass extra sanity check (e.g. email address)
+        """
+        # super-admin can be used to bypass extra checks
+        if self.env.user._is_admin():
+            extra_checks = False
+
+        Partner = self.env['res.partner']
+        partner_ids = Partner.browse(partner_ids).exists()
+        if len(partner_ids) < 2:
+            return
+
+        if len(partner_ids) > 50:
+            raise UserError(_("For safety reasons, you cannot merge more than 50 contacts together. You can re-open the wizard several times if needed."))
+
+        # check if the list of partners to merge contains child/parent relation
+        child_ids = self.env['res.partner']
+        for partner_id in partner_ids:
+            child_ids |= Partner.search([('id', 'child_of', [partner_id.id])]) - partner_id
+        if partner_ids & child_ids:
+            raise UserError(_("You cannot merge a contact with one of his parent."))
+
+        if extra_checks and len(set(partner.email for partner in partner_ids)) > 1:
+            raise UserError(_("All contacts must have the same email. Only the Administrator can merge contacts with different emails."))
+
+        # remove dst_partner from partners to merge
+        if dst_partner and dst_partner in partner_ids:
+            src_partners = partner_ids - dst_partner
+        else:
+            ordered_partners = self._get_ordered_partner(partner_ids.ids)
+            dst_partner = ordered_partners[-1]
+            src_partners = ordered_partners[:-1]
+        _logger.info("dst_partner: %s", dst_partner.id)
+
+        # FIXME: is it still required to make and exception for account.move.line since accounting v9.0 ?
+        if extra_checks and 'account.move.line' in self.env and self.env['account.move.line'].sudo().search([('partner_id', 'in', [partner.id for partner in src_partners])]):
+            raise UserError(_("Only the destination contact may be linked to existing Journal Items. Please ask the Administrator if you need to merge several contacts linked to existing Journal Items."))
+
+        # Make the company of all related users consistent with destination partner company
+        if dst_partner.company_id:
+            partner_ids.mapped('user_ids').sudo().write({
+                'company_ids': [(4, dst_partner.company_id.id)],
+                'company_id': dst_partner.company_id.id
+            })
+
+        # call sub methods to do the merge
+        self._update_foreign_keys(src_partners, dst_partner)
+        self._update_reference_fields(src_partners, dst_partner)
+        self._update_values(src_partners, dst_partner)
+
+        self._log_merge_operation(src_partners, dst_partner)
+
+        # delete source partner, since they are merged
+        src_partners.unlink()
 
     @api.model
     def _generate_query(self, fields, maximum_group=100):
