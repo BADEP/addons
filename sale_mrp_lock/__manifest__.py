1eb1e83c42928c185943ae20ededd2e69094d27d
Khalid Hazam <k.hazam@badep.ma>
2021-07-04 23:32:53 +0100

    [FF] sale_mrp_lock

---
+++ sale_mrp_lock/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Sale lock related production orders',
+    'summary': 'Lock productions of sale when it is locked',
+    'version': '13.0.1.1.1',
+    'category': 'Generic Modules/Sale',
+    'author': "BADEP",
+    'depends': ['mrp_sale_info'],
+    'license': 'AGPL-3',
+    'data': [
+    ],
+    'installable': False
+}
