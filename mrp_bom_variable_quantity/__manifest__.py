87e422e56518cb99f58e433718a3a9bd76ea36c3
Khalid Hazam <k.hazam@badep.ma>
2021-07-04 21:49:07 +0100

    [FF] mrp_bom_variable_quantity

--- mrp_bom_variable_quantity/__manifest__.py
+++ mrp_bom_variable_quantity/__manifest__.py
@@ -1,19 +1,14 @@
-# Copyright 2016-2019 Tecnativa - Pedro M. Baeza
-# Copyright 2018 Tecnativa - Carlos Dauden
-# Copyright 2019 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 {
     'name': 'Variable quantity in production',
-    'version': '12.0.3.0.0',
+    'version': '13.0.3.0.0',
     'license': 'AGPL-3',
-    'author': "OCA, BADEP, Pragmatic System",
-    'website': 'https://www.tecnativa.com',
+    'author': "OCA, BADEP",
+    'website': 'https://badep.ma',
     'depends': ['mrp'],
     'data': [
         'security/ir.model.access.csv',
         'views/mrp_bom_line_formula_view.xml',
         'views/mrp_bom_view.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
