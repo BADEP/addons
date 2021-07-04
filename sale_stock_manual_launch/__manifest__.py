8384b73a405b3dbb5947cab2de8268394e53afa3
Khalid Hazam <k.hazam@badep.ma>
2021-07-04 23:55:09 +0100

    [FF] sale_stock_manual_launch

---
+++ sale_stock_manual_launch/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'Sale Stock Manual Lauch Procurement',
+    'version': '13.0',
+    'category': 'Sale',
+    'author': 'BADEP',
+    'description': """
+    """,
+    'depends': ['sale_stock'],
+    'data': [
+             'wizards/launch_procurement_wizard_view.xml',
+             'views/views.xml',
+    ],
+    'installable' : True,
+}
