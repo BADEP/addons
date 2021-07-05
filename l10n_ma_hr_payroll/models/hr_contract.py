b084495b0168e8bab454aba969517424cce101c2
Khalid Hazam <k.hazam@badep.ma>
2021-07-05 13:56:57 +0100

    [FF] l10n_ma_hr_payroll

--- l10n_ma_hr_payroll/models/hr_contract.py
+++ l10n_ma_hr_payroll/models/hr_contract.py
@@ -4,13 +4,13 @@ from odoo.addons import decimal_precision as dp
 class HrContract(models.Model):
     _inherit = 'hr.contract'
 
-    int1 = fields.Float(digits=dp.get_precision('Account'), string="Indemnité Non taxable 1")
-    int2 = fields.Float(digits=dp.get_precision('Account'), string="Indemnité Non taxable 2")
-    int3 = fields.Float(digits=dp.get_precision('Account'), string="Indemnité Non taxable 3")
-    int4 = fields.Float(digits=dp.get_precision('Account'), string="Indemnité Non taxable 4")
-    int5 = fields.Float(digits=dp.get_precision('Account'), string="Indemnité Non taxable 5")
-    it1 = fields.Float(digits=dp.get_precision('Account'), string="Indemnité taxable 1")
-    it2 = fields.Float(digits=dp.get_precision('Account'), string="Indemnité taxable 2")
-    it3 = fields.Float(digits=dp.get_precision('Account'), string="Indemnité taxable 3")
-    it4 = fields.Float(digits=dp.get_precision('Account'), string="Indemnité taxable 4")
-    it5 = fields.Float(digits=dp.get_precision('Account'), string="Indemnité taxable 5")
+    int1 = fields.Float(digits='Account', string="Indemnité Non taxable 1")
+    int2 = fields.Float(digits='Account', string="Indemnité Non taxable 2")
+    int3 = fields.Float(digits='Account', string="Indemnité Non taxable 3")
+    int4 = fields.Float(digits='Account', string="Indemnité Non taxable 4")
+    int5 = fields.Float(digits='Account', string="Indemnité Non taxable 5")
+    it1 = fields.Float(digits='Account', string="Indemnité taxable 1")
+    it2 = fields.Float(digits='Account', string="Indemnité taxable 2")
+    it3 = fields.Float(digits='Account', string="Indemnité taxable 3")
+    it4 = fields.Float(digits='Account', string="Indemnité taxable 4")
+    it5 = fields.Float(digits='Account', string="Indemnité taxable 5")
