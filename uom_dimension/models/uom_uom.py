f47eba43e3ba4afdc823d97b7ae0fe82e86aeefc
Khalid Hazam <k.hazam@badep.ma>
2021-07-05 13:02:45 +0100

    [FF] uom_dimension

--- uom_dimension/models/uom_uom.py
+++ uom_dimension/models/uom_uom.py
@@ -1,6 +1,6 @@
 from odoo import models, fields, api, exceptions, _
 import time, datetime, dateutil, numpy
-from odoo.tools.safe_eval import safe_eval
+from odoo.tools import safe_eval
 
 DEFAULT_PYTHON_CODE = """# Available variables:
 #  - env: Odoo Environment
@@ -13,14 +13,15 @@ DEFAULT_PYTHON_CODE = """# Available variables:
 #  Example (Total perimeter of a window with regard of the quantity wanted): result = 2 * dimension_values[dimension_ids[0].id] * (dimension_values[dimension_ids[1].id] + dimension_values[dimension_ids[2].id])
 #  Example (Total surface of a window with regard of the quantity wanted): result = numpy.prod(dimension_values.values()) \n\n\n\n"""
 
+
 class UomUom(models.Model):
     _inherit = 'uom.uom'
-    
+
     dimension_ids = fields.One2many('uom.dimension', 'parent_uom_id', string='Dimensions', copy=True)
     calculation_type = fields.Selection([('simple', 'Simple'), ('code', 'Code')], default='simple', required=True, string='Calculation Type')
     code = fields.Text(string='Python Code', default=DEFAULT_PYTHON_CODE)
 
-    def eval_values(self, dimension_values, product_dimension_qty = 1):
+    def eval_values(self, dimension_values, product_dimension_qty=1):
         for uom in self:
             if uom.calculation_type == 'simple':
                 code = 'result = product_dimension_qty * numpy.prod(list(dimension_values.values()))'
@@ -28,10 +29,10 @@ class UomUom(models.Model):
                 code = uom.code
             eval_context = {
                 'env': self.env,
-                'time': time,
-                'datetime': datetime,
-                'dateutil': dateutil,
-                'numpy': numpy,
+                'datetime': safe_eval.datetime,
+                'dateutil': safe_eval.dateutil,
+                'time': safe_eval.time,
+                'numpy': safe_eval.wrap_module(__import__('numpy'), ['prod']),
                 'Warning': exceptions.Warning,
                 'record': uom,
                 'product_dimension_qty': product_dimension_qty,
@@ -39,10 +40,11 @@ class UomUom(models.Model):
                 'result': 0,
             }
             # try:
-            safe_eval(code, eval_context, mode="exec", nocopy=True)
+            safe_eval.safe_eval(code, eval_context, mode="exec", nocopy=True)
             return float(eval_context['result'])
             # except, e:
             #     raise exceptions.UserError(_('Wrong python code defined for uom %s.') % (uom.name))
+
 
 class UomDimension(models.Model):
     _name = 'uom.dimension'
