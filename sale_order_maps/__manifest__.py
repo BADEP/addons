42c607d0bf6aa4ca80ad160221448584d6a21149
Khalid Hazam <k.hazam@badep.ma>
2021-07-06 12:11:23 +0100

    [FF] sale_order_maps

--- sale_order_maps/__manifest__.py
+++ sale_order_maps/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Sale Order Maps',
-    'version': '13.0.1.0.0',
+    'version': '14.0.1.0.0',
     'author': 'Yopi Angi, BADEP',
     'license': 'AGPL-3',
     'maintainer': 'Yopi Angi <yopiangi@gmail.com>, Khalid Hazam <k.hazam@badep.ma>',
