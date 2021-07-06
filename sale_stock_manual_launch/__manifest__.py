eccd4745d39eb76270f8193e1b6fe8e18ff5eab1
Khalid Hazam <k.hazam@badep.ma>
2021-07-06 19:19:00 +0100

    [IMP] sale_stock_manual_launch pub to store

--- sale_stock_manual_launch/__manifest__.py
+++ sale_stock_manual_launch/__manifest__.py
@@ -1,17 +1,21 @@
-# -*- coding: utf-8 -*-
-
 {
     'name' : 'Sale Stock Manual Lauch Procurement',
-    'version': '14.0',
-    'category': 'Sale',
+    'summary': 'Manually specify quantity to send to procurement in sales',
+    'description': 'Manually specify quantity to send to procurement in sales',
+    'version': '14.0.1.1',
+    'category': 'Sales',
     'author': 'BADEP',
-    'description': """
-    """,
+    'website': "https://badep.ma",
+    'images': ['static/src/img/banner.png'],
+    'license': 'AGPL-3',
+
     'depends': ['sale_stock'],
     'data': [
              'security/ir.model.access.csv',
              'wizards/launch_procurement_wizard_view.xml',
              'views/views.xml',
     ],
-    'installable' : True,
+
+    'price': 99.00,
+    'currency': 'EUR',
 }
--- sale_stock_manual_launch/wizards/launch_procurement_wizard.py
+++ sale_stock_manual_launch/wizards/launch_procurement_wizard.py
@@ -2,16 +2,17 @@ from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
 from odoo.addons import decimal_precision as dp
 
+
 class LaunchProcurementWizard(models.TransientModel):
     _name = 'launch.procurement.wizard'
     _description = 'Launch procurement wizard'
 
-    line_ids = fields.One2many('launch.procurement.wizard.line', 'wizard_id', 'Lignes')
+    line_ids = fields.One2many('launch.procurement.wizard.line', 'wizard_id', 'Lines')
 
     @api.model
     def default_get(self, fields):
         if len(self.env.context.get('active_ids', list())) > 1:
-            raise UserError(_("Vous ne pouvez créer des approvisionnements pour une commande à la fois."))
+            raise UserError(_("You cannot launch procurement for more than one Sale Order."))
         res = super(LaunchProcurementWizard, self).default_get(fields)
 
         so = self.env['sale.order'].browse(self.env.context.get('active_id'))
@@ -22,9 +23,9 @@ class LaunchProcurementWizard(models.TransientModel):
             for line in so.order_line.filtered(lambda l: l.to_launch):
                 line_data = dict(sale_order_line_id)
                 line_data.update({
-                        'sale_order_line_id': line.id,
-                        'quantity': line.get_dummy_qty(),
-                    })
+                    'sale_order_line_id': line.id,
+                    'quantity': line.get_dummy_qty(),
+                })
                 line_ids.append((0, 0, line_data))
             if 'line_ids' in fields:
                 res.update({'line_ids': line_ids})
@@ -34,17 +35,18 @@ class LaunchProcurementWizard(models.TransientModel):
         for line in self.line_ids:
             line.sale_order_line_id.with_context(qty_to_launch=line.quantity).action_launch_procurement()
 
+
 class LaunchProcurementWizardLine(models.TransientModel):
     _name = 'launch.procurement.wizard.line'
     _description = 'Launch procurement wizard line'
 
     wizard_id = fields.Many2one('launch.procurement.wizard', string="Wizard")
-    sale_order_line_id = fields.Many2one('sale.order.line', string="Ligne de commande")
-    quantity = fields.Float(string="Quantité", digits='Product Unit of Measure')
+    sale_order_line_id = fields.Many2one('sale.order.line', string="Sale Order Line")
+    quantity = fields.Float(string="Quantity", digits='Product Unit of Measure')
 
     @api.constrains('quantity')
     def _check_max_qty(self):
         for rec in self:
             if rec.quantity > rec.sale_order_line_id.product_uom_qty:
-                raise ValidationError("Vous ne pouvez pas lancer en approvisionnement plus que la quantité restante: %s" % (rec.sale_order_line_id.product_uom_qty - rec.sale_order_line_id.procurement_qty))
-
+                raise ValidationError(_("You cannot launch more than the remaining quantity: %s (%s)" % (
+                rec.sale_order_line_id.display_name, rec.sale_order_line_id.product_uom_qty - rec.sale_order_line_id.procurement_qty)))
