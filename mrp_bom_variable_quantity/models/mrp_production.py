0fae11a06495926e4fc6a6f51c6f4ca1c8086108
Khalid Hazam <k.hazam@badep.ma>
2021-07-06 11:18:02 +0100

    [FIX] mrp_bom_variable_qty

--- mrp_bom_variable_quantity/models/mrp_production.py
+++ mrp_bom_variable_quantity/models/mrp_production.py
@@ -7,3 +7,10 @@ class MrpProduction(models.Model):
         for production in self:
             super(MrpProduction, self.with_context(active_model='mrp.production', active_id=production.id))._generate_moves()
         return True
+
+    #Force move raw recalculation since formulas are not linear
+    #Todo: compare _update_raw_moves and _onchange_move_raw and add diff as new raw moves
+    # def _update_raw_moves(self, factor):
+    #     self.ensure_one()
+    #     self._onchange_move_raw()
+    #     return []
