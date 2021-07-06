6bcdfa74be952f60b66ba228125a7816fbaabb21
Khalid Hazam <k.hazam@badep.ma>
2021-07-06 20:38:58 +0100

    [IMP] crm_facebook_leads cleanup

--- crm_facebook_leads/__manifest__.py
+++ crm_facebook_leads/__manifest__.py
@@ -1,28 +1,22 @@
-# -*- coding: utf-8 -*-
 {
     'name': "CRM Facebook Lead Ads",
-
     'summary': """
         Sync Facebook Leads with Odoo CRM""",
 
     'description': """
     """,
-
     'author': "BADEP, Vauxoo",
     'website': "https://badep.ma",
-
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Lead Automation',
     'version': '14.0.2.0.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['crm'],
     'images': ['static/src/img/banner.png'],
     'license': 'AGPL-3',
 
-    # always loaded
     'data': [
         'data/ir_cron.xml',
         'data/crm.facebook.form.mapping.csv',
