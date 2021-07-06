ad3b3a4c9e9adab4ef929ac9a42e60949690723f
Khalid Hazam <k.hazam@badep.ma>
2021-07-06 21:10:04 +0100

    [PUB] sale_maps

--- sale_maps/models/sale.py
+++ sale_maps/models/sale.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3
 from odoo import api, fields, models
-from odoo.addons.base_geolocalize.models.res_partner import (
-    geo_find, geo_query_address)
-
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -13,26 +8,29 @@ class SaleOrder(models.Model):
     shipping_latitude = fields.Float(string='Shipping Latitude', related='partner_shipping_id.partner_latitude',
                                      store=False, readonly=False, digits=(16, 5))
 
+    @api.model
+    def _geo_localize(self, street='', zip='', city='', state='', country=''):
+        geo_obj = self.env['base.geocoder']
+        search = geo_obj.geo_query_address(
+            street=street, zip=zip, city=city, state=state, country=country
+        )
+        result = geo_obj.geo_find(search, force_country=country)
+        if result is None:
+            search = geo_obj.geo_query_address(
+                city=city, state=state, country=country
+            )
+            result = geo_obj.geo_find(search, force_country=country)
+        return result
+
+
     def geo_localize(self):
-        google_api_key = self.env['ir.config_parameter'].sudo().get_param(
-            'google.api_key_geocode', default='')
         for sale in self.with_context(lang='en_US'):
-            result = geo_find(
-                addr=geo_query_address(
+            result = self._geo_localize(
                     street=sale.partner_shipping_id.street,
                     zip=sale.partner_shipping_id.zip,
                     city=sale.partner_shipping_id.city,
                     state=sale.partner_shipping_id.state_id.name,
-                    country=sale.partner_shipping_id.country_id.name),
-                apikey=google_api_key)
-
-            if result is None:
-                result = geo_find(
-                    addr=geo_query_address(
-                        city=sale.partner_shipping_id.city,
-                        state=sale.partner_shipping_id.state_id.name,
-                        country=sale.partner_shipping_id.country_id.name),
-                    apikey=google_api_key)
+                    country=sale.partner_shipping_id.country_id.name)
 
             if result:
                 sale.write({
