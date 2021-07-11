73f26fcaffcc2fa9d11b14899597528cdc31c19c
Khalid Hazam <k.hazam@badep.ma>
2021-07-11 15:53:57 +0100

    [ADD] base_widget_remaining_days_color

---
+++ base_widget_remaining_days_color/static/src/js/remaining_days_widget.js
@@ -0,0 +1,21 @@
+odoo.define('base_widget_remaining_days_color.basic_fields', function (require) {
+    "use strict";
+    var session = require('web.session');
+    const RemainingDays = require('web.basic_fields').RemainingDays;
+
+    RemainingDays.include({
+        _render: function () {
+            this._super();
+            const nowUTC = moment().utc();
+            const nowUserTZ = nowUTC.clone().add(session.getTZOffset(nowUTC), 'minutes');
+            const valueUserTZ = this.value.clone().add(session.getTZOffset(this.value), 'minutes');
+            const diffDays = valueUserTZ.startOf('day').diff(nowUserTZ.startOf('day'), 'days');
+            this.$el.toggleClass('text-success', diffDays > 0);
+        }
+    });
+
+    return {
+        RemainingDays: RemainingDays,
+    };
+
+});
