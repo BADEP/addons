ab149088197ea666ce4b7f0be49f99115edaeb90
Khalid Hazam <k.hazam@badep.ma>
2021-07-11 15:53:57 +0100

    [ADD] base_widget_remaining_days_color

---
+++ base_widget_remaining_days_color/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Remaining Days Widget",
+    'summary': """Add green color if remaining days are positive""",
+    'description': """
+    """,
+    'author': "BADEP",
+    'website': "https://badep.ma",
+    'category': 'Technical',
+    'version': '14.0.0.0.1',
+
+    'depends': ['base'],
+    'images': ['static/src/img/banner.png'],
+    'license': 'AGPL-3',
+
+    'data': [
+        'views/asset_views.xml',
+    ],
+    'price': 9.00,
+    'currency': 'EUR',
+}
