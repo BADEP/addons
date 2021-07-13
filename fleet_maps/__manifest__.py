7c05dfea7a540c49fee438d2fb9111251063ae8b
Khalid Hazam <k.hazam@badep.ma>
2021-07-13 12:39:24 +0100

    [IMP] add website to fleet_maps

--- fleet_maps/__manifest__.py
+++ fleet_maps/__manifest__.py
@@ -4,6 +4,7 @@
     'version': '14.0.1.0.2',
     'author': 'Yopi Angi, BADEP',
     'license': 'AGPL-3',
+    'website': 'https://badep.ma',
     'maintainer': 'Yopi Angi <yopiangi@gmail.com>, Khalid Hazam <k.hazam@badep.ma>',
     'support': 'yopiangi@gmail.com',
     'category': 'Fleet',
