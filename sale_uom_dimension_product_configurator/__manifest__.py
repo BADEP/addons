27d0e57809ec1eee21ad54eebcbc9203ffea8391
Khalid Hazam <k.hazam@badep.ma>
2021-08-06 10:36:31 +0100

    [WIP] sale_uom_dimension_product_configurator

---
+++ sale_uom_dimension_product_configurator/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': "UoM Dimensions in Sales - Product Configurator",
+
+    'summary': """
+        Allows the use of UoM dimensions in sales with product configurator.""",
+    'author': "BADEP",
+    'website': "https://badep.ma",
+    'category': 'Sales Management',
+    'version': '14.0.1.0.1',
+    'images': ['static/src/img/banner.png'],
+    'license': 'AGPL-3',
+    'depends': ['sale_product_configurator', 'sale_uom_dimension'],
+    'data': [
+             'views/templates.xml',
+             'views/sale_views.xml',
+             'views/assets.xml',
+             ],
+    'auto_install': True,
+    'installable': False,
+    'price': 30,
+    'currency': 'EUR'
+}
