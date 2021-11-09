4bbfb316dceff2433c3075673cdc89c357bc16eb
Khalid Hazam <k.hazam@badep.ma>
2021-11-09 09:13:48 +0100

    [IMP] purchase_total_delivered_invoiced proper inheritance

--- purchase_total_delivered_invoiced/models/purchase.py
+++ purchase_total_delivered_invoiced/models/purchase.py
@@ -48,43 +48,4 @@ class PurchaseReport(models.Model):
             )""" % (self._table, self._select(), self._from(), self._group_by()))
 
     def _select(self):
-        select_str = """
-            WITH currency_rate as (%s)
-                SELECT
-                    po.id as order_id,
-                    min(l.id) as id,
-                    po.date_order as date_order,
-                    po.state,
-                    po.date_approve,
-                    po.dest_address_id,
-                    po.partner_id as partner_id,
-                    po.user_id as user_id,
-                    po.company_id as company_id,
-                    po.fiscal_position_id as fiscal_position_id,
-                    l.product_id,
-                    p.product_tmpl_id,
-                    t.categ_id as category_id,
-                    po.currency_id,
-                    t.uom_id as product_uom,
-                    extract(epoch from age(po.date_approve,po.date_order))/(24*60*60)::decimal(16,2) as delay,
-                    extract(epoch from age(l.date_planned,po.date_order))/(24*60*60)::decimal(16,2) as delay_pass,
-                    count(*) as nbr_lines,
-                    sum(l.price_total / COALESCE(po.currency_rate, 1.0))::decimal(16,2) as price_total,
-                    sum(l.price_received / COALESCE(po.currency_rate, 1.0))::decimal(16,2) as price_received,
-                    sum(l.price_invoiced / COALESCE(po.currency_rate, 1.0))::decimal(16,2) as price_invoiced,
-                    (sum(l.product_qty * l.price_unit / COALESCE(po.currency_rate, 1.0))/NULLIF(sum(l.product_qty/line_uom.factor*product_uom.factor),0.0))::decimal(16,2) as price_average,
-                    partner.country_id as country_id,
-                    partner.commercial_partner_id as commercial_partner_id,
-                    analytic_account.id as account_analytic_id,
-                    sum(p.weight * l.product_qty/line_uom.factor*product_uom.factor) as weight,
-                    sum(p.volume * l.product_qty/line_uom.factor*product_uom.factor) as volume,
-                    sum(l.price_subtotal / COALESCE(po.currency_rate, 1.0))::decimal(16,2) as untaxed_total,
-                    sum(l.product_qty / line_uom.factor * product_uom.factor) as qty_ordered,
-                    sum(l.qty_received / line_uom.factor * product_uom.factor) as qty_received,
-                    sum(l.qty_invoiced / line_uom.factor * product_uom.factor) as qty_billed,
-                    case when t.purchase_method = 'purchase' 
-                         then sum(l.product_qty / line_uom.factor * product_uom.factor) - sum(l.qty_invoiced / line_uom.factor * product_uom.factor)
-                         else sum(l.qty_received / line_uom.factor * product_uom.factor) - sum(l.qty_invoiced / line_uom.factor * product_uom.factor)
-                    end as qty_to_be_billed
-        """ % self.env['res.currency']._select_companies_rates()
-        return select_str
+        return super()._select() + ", sum(l.qty_received / line_uom.factor * product_uom.factor) as qty_received, sum(l.qty_invoiced / line_uom.factor * product_uom.factor) as qty_billed"
