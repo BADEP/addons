e215d85a369e489bc73e7c89dccbae442447e804
Khalid Hazam <k.hazam@badep.ma>
2021-11-29 23:34:29 +0100

    [IMP] stock_uom_dimension gather only available dimensions

--- stock_uom_dimension/models/stock_quant.py
+++ stock_uom_dimension/models/stock_quant.py
@@ -26,8 +26,9 @@ class StockQuant(models.Model):
     def _gather(self, product_id, location_id, lot_id=None, package_id=None, owner_id=None, strict=False):
         quants = super(StockQuant, self)._gather(product_id, location_id, lot_id, package_id, owner_id, strict)
         if self.env.context.get('dimension_ids') and product_id.stock_dimensions_strict:
-            return quants.filtered(lambda q: q.dimension_ids.mapped('dimension_id').ids == list(self.env.context.get('dimension_ids').keys()) and
-                                             all(d.quantity == self.env.context['dimension_ids'].get(d.dimension_id.id, 0) for d in q.dimension_ids))
+            return quants.filtered(lambda q:
+                                   all(d in self.env.context.get('dimension_ids').keys() for d in q.dimension_ids.mapped('dimension_id').ids) and
+                                   all(d.quantity == self.env.context['dimension_ids'].get(d.dimension_id.id, 0) for d in q.dimension_ids))
         return quants
 
 
