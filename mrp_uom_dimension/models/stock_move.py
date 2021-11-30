c6587ec5c66c57a9014823fb50c9dc7721048d4f
Khalid Hazam <k.hazam@badep.ma>
2021-11-30 12:01:31 +0100

    [FIX] mrp_uom_dimension wrong parameter

--- mrp_uom_dimension/models/stock_move.py
+++ mrp_uom_dimension/models/stock_move.py
@@ -9,4 +9,4 @@ class StockMove(models.Model):
                 super(StockMove, rec.with_context(dimension_ids={d.dimension_id.id: d.quantity for d in rec.raw_material_production_id.dimension_ids},
                                                   product_dimension_qty=rec.raw_material_production_id.product_dimension_qty))._action_assign()
             else:
-                super()._action_assign(self)
+                super()._action_assign()
