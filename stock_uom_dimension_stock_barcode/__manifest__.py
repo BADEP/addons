0e2b1ed5d932a8cfa799e132f2969b645fc99b0b
Khalid Hazam <k.hazam@badep.ma>
2022-01-31 23:11:46 +0100

    [ADD] stock_uom_dimension_stock_barcode

---
+++ stock_uom_dimension_stock_barcode/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Product dimensions in stock_barcode Entreprise module',
+    'version': '14.0',
+    'category': 'Inventory',
+    'license': 'AGPL-3',
+    'description': """
+    This module allow to have multidimensional UoMs in stock_barcode module.
+    """,
+    'author': 'BADEP',
+    'website': 'http://www.badep.ma',
+    'depends': ['stock_barcode', 'stock_uom_dimension'],
+    'data': [
+             'views/stock_move_line_views.xml',
+             ],
+    'installable': True,
+    'auto_install': True,
+    'price': 50,
+    'currency': 'EUR'
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
