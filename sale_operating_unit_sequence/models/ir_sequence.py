76bd1c44945425268f114e1dec7bc5f4e3fd21a7
g <ghoostinger@mail.com>
2022-03-07 12:29:24 +0000

    [WIP] sale_operating_unit_sequence ir_sequence remove next_by_code

--- sale_operating_unit_sequence/models/ir_sequence.py
+++ sale_operating_unit_sequence/models/ir_sequence.py
@@ -11,33 +11,41 @@ class IrSequence(models.Model):
 
     operating_unit_id = fields.Many2one('operating.unit')
 
-    @api.model
-    def next_by_code(self, sequence_code):
-        """ Draw an interpolated string using a sequence with the requested code.
-            If several sequences with the correct code are available to the user
-            (multi-company cases), the one from the user's current company will
-            be used.
-
-            :param dict context: context dictionary may contain a
-                ``force_company`` key with the ID of the company to
-                use instead of the user's current company for the
-                sequence selection. A matching sequence for that
-                specific company will get higher priority.
-        """
-        self.check_access_rights('read')
-        operating_unit_id = self._context.get('operating_unit_id')
-        if operating_unit_id:
-            force_company = self._context.get('force_company')
-            if not force_company:
-                force_company = self.env.user.company_id.id
-            seq_ids = self.search(
-                [('code', '=', sequence_code), ('operating_unit_id', 'in', [operating_unit_id, False]),
-                 ('company_id', 'in', [force_company, False])], order='operating_unit_id,company_id')
-            if not seq_ids:
-                _logger.debug(
-                    "No ir.sequence has been found for code '%s'. Please make sure a sequence is set for current operating unit." % sequence_code)
-                return False
-            seq_id = seq_ids[0]
-            return seq_id._next()
-        else:
-            return super().next_by_code(sequence_code)
+    # @api.model
+    # def next_by_code(self, sequence_code):
+    #     """ Draw an interpolated string using a sequence with the requested code.
+    #         If several sequences with the correct code are available to the user
+    #         (multi-company cases), the one from the user's current company will
+    #         be used.
+    #
+    #         :param dict context: context dictionary may contain a
+    #             ``force_company`` key with the ID of the company to
+    #             use instead of the user's current company for the
+    #             sequence selection. A matching sequence for that
+    #             specific company will get higher priority.
+    #     """
+    #     self.check_access_rights('read')
+    #     operating_unit_id = self._context.get('operating_unit_id')
+    #     if operating_unit_id:
+    #         force_company = self._context.get('force_company')
+    #         if not force_company:
+    #             force_company = self.env.user.company_id.id
+    #         seq_ids = self.search(
+    #             [('code', '=', sequence_code), ('operating_unit_id', 'in', [operating_unit_id, False]),
+    #              ('company_id', 'in', [force_company, False])], order='operating_unit_id,company_id')
+    #         if not seq_ids:
+    #             _logger.debug(
+    #                 "No ir.sequence has been found for code '%s'. Please make sure a sequence is set for current operating unit." % sequence_code)
+    #             return False
+    #         seq_id = seq_ids[0]
+    #         return seq_id._next()
+    #     else:
+    #         return super().next_by_code(sequence_code)
+    #
+    # @api.model
+    # def next_by_code(self, sequence_code, sequence_date=None):
+    #     sequence_id = self.env.context.get("sequence_option_id", False)
+    #     if sequence_id:
+    #         self = self.browse(sequence_id)
+    #         return super().next_by_id(sequence_date=sequence_date)
+    #     return super().next_by_code(sequence_code, sequence_date=sequence_date)
