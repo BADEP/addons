46b3475f613718d6a5cc5cab3095425a4d269e43
Khalid Hazam <k.hazam@badep.ma>
2022-03-25 14:14:52 +0100

    [ADD] base_action_random

---
+++ base_action_random/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "random in server actions",
+
+    'summary': """Allow the use of xmlrpc lib in server actions
+        """,
+
+    'description': """
+    """,
+
+    'author': "BADEP",
+    'website': "https://badep.ma",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Technical Settings',
+    'version': '14.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base'
+    ],
+
+    'installable': True,
+    'auto_install': True,
+}
