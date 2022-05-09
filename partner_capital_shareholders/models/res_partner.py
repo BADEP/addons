23725bb57513815ff8c0776ef880dd4b4c58f1d7
Khalid Hazam <k.hazam@badep.ma>
2022-05-09 18:00:41 +0100

    [IMP] partner_capital_shareholders parts amount calculation

--- partner_capital_shareholders/models/res_partner.py
+++ partner_capital_shareholders/models/res_partner.py
@@ -11,4 +11,12 @@ class ResPartnerPart(models.Model):
 
     partner_id = fields.Many2one('res.partner', string='Shareholder', required=True, ondelete='cascade')
     parent_partner_id = fields.Many2one('res.partner', string='Company', required=True, ondelete='cascade')
-    part = fields.Float(string='Part', required=True)
+    part = fields.Float(string='Parts', required=True)
+    part_amount = fields.Float(string='Part Amount', compute = '_part_amount')
+    type = fields.Selection([('internal', 'Interne'), ('external', 'Externe')])
+
+    @api.depends('part', 'parent_partner_id.capital_amount', 'parent_partner_id.part_ids', 'parent_partner_id.part_ids.part')
+    def _part_amount(self):
+        for rec in self:
+            all_parts = sum(rec.parent_partner_id.part_ids.mapped('part'))
+            rec.part_amount = 0 if all_parts == 0 else (rec.parent_partner_id.capital_amount * rec.part / all_parts)
