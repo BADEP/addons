7e477d9ad44c8133609ad52ca1a0ecbaeec1da95
Khalid Hazam <k.hazam@badep.ma>
2022-05-09 17:11:19 +0100

    [ PUB] partner_capital_shareholders

--- partner_shareholders/models/res_company.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields, api
-
-class ResCompany(models.Model):
-    _inherit = 'res.company'
-
-    part_ids = fields.One2many('res.partner.part', related='partner_id.part_ids', string='Shareholders Parts')
