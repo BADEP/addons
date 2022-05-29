88164967235de176146bda00ef58e898a62fa893
Khalid Hazam <k.hazam@badep.ma>
2022-05-29 15:11:27 +0100

    [ADD] base_upgrade_path

---
+++ base_upgrade_path/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Check upgrade path",
+
+    'summary': """Allow to check if module is upgradable in target version
+        """,
+
+    'description': """
+    """,
+
+    'author': "BADEP",
+    'website': "https://badep.ma",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Technical Settings',
+    'version': '12.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base'
+    ],
+    'data': [
+        'views/ir_module_module_views.xml',
+        'security/ir.model.access.csv'
+     ],
+    'installable': True,
+    'auto_install': True,
+}
