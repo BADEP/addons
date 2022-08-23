45bde31f5267d1c42ecec7d3474cfdb07cc7cef4
kh-badep <k.hazam@badep.ma>
2022-08-23 15:46:18 +0100

    [ADD] account_journal_type

---
+++ account_journal_type/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Account Journal User type",
+
+    'summary': """
+        Add user-defined types to journals to help group journal by their nature (Payroll, etc.)""",
+
+    'description': """
+        
+    """,
+
+    'author': "BADEP",
+    'website': "https://badep.ma",
+
+    'category': 'Invoicing &amp; Payments',
+    'version': '14.0.1.0',
+
+    'depends': ['account'],
+    'images': ['static/src/img/banner.png'],
+    'license': 'AGPL-3',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/account_journal_views.xml',
+        'views/account_journal_type_views.xml',
+    ],
+    'installable': True,
+    'price': 49.00,
+    'currency': 'EUR',
+}
