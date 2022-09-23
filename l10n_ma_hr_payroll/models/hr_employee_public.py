7f217e3971c0daa9dc89748bec4c7c3f2934b355
Ghizlane Rouinab <g.rouinab@badep.ma>
2022-09-23 13:16:55 +0100

    [IMP] l10n_ma_hr_payroll add missing fields

--- l10n_ma_hr_payroll/models/hr_employee_public.py
+++ l10n_ma_hr_payroll/models/hr_employee_public.py
@@ -4,3 +4,6 @@ class HrEmployeePublic(models.Model):
     _inherit = 'hr.employee.public'
 
     cin = fields.Char(string="Numéro CIN", required=False)
+    matricule_cnss = fields.Char(string="Numéro CNSS", required=False)
+    matricule_cimr = fields.Char(string="Numéro CIMR", required=False)
+    matricule_mut = fields.Char(string="Numéro MUTUELLE", required=False)
