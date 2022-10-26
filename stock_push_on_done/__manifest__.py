c04cff27470ea2e77f24d59846c339c8b2aac7cc
kh-badep <k.hazam@badep.ma>
2022-10-26 21:38:09 +0100

    [WIP] frozentrade_meta

--- stock_push_on_done/__manifest__.py
+++ stock_push_on_done/__manifest__.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Stock Fleet",
+    'name': "Stock Push On done moves",
 
-    'summary': """
-        Link Stock with Fleet""",
+    'summary': """""",
 
-    'description': """
-        Add vehicles and drivers assignation on Picking Orders and waves. These Statistics can also be accessed directly from the vehicle view.
-    """,
+    'description': """""",
 
     'author': "BADEP",
     'website': "https://badep.ma",
@@ -22,7 +19,7 @@
     'depends': ['stock'],
     'images': ['static/src/img/banner.png'],
     'license': 'AGPL-3',
-    'installable': False,
+     'installable': True,
 
     # always loaded
     'data': [
