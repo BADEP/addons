bf388e3c619580b75bb3d7314724fd53d0d2c90f
kh-badep <k.hazam@badep.ma>
2022-12-16 10:49:37 +0100

    [FIX] purchase_request_uom_dimension do not group dimensions

--- purchase_request_uom_dimension/wizard/purchase_request_line_make_purchase_order.py
+++ purchase_request_uom_dimension/wizard/purchase_request_line_make_purchase_order.py
@@ -11,3 +11,10 @@ class PurchaseRequestLineMakePurchaseOrder(models.TransientModel):
                 'dimension_ids': [(0, 0, {'dimension_id': d.dimension_id.id, 'quantity': d.quantity}) for d in item.line_id.dimension_ids]
             })
         return res
+
+    @api.model
+    def _get_order_line_search_domain(self, order, item):
+        res = super()._get_order_line_search_domain(order, item)
+        if item.dimension_ids:
+            res.append((1, "=", 0))
+        return res
